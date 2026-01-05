32e1a8518a0506a14e9edd9ad3f40665625213ce
ruby-automation <171511148+ruby-automation@users.noreply.github.com>
2026-01-05 16:40:53 -0600

    chore(main): release 0.1.0 (#16)

--- lib/kalshi/version.rb
+++ lib/kalshi/version.rb
@@ -4,7 +4,7 @@ module Rubyists
   # Kalshi module version
   module Kalshi
     # x-release-please-start-version
-    VERSION = '0.0.5'
+    VERSION = '0.1.0'
     # x-release-please-end
   end
 end
